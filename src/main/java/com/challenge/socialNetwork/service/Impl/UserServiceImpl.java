9b6d41cb298869f0ab4ebb38081b2a8d27947b18
reihaneh <reihaneh763@gmail.com>
2023-04-12 14:16:44 +0430

    add UserServiceImpl class

--- src/main/java/com/challenge/socialNetwork/service/Impl/UserServiceImpl.java
+++ src/main/java/com/challenge/socialNetwork/service/Impl/UserServiceImpl.java
@@ -1,2 +1,26 @@
-package com.challenge.socialNetwork.service.Impl;public class UserServiceImpl {
+package com.challenge.socialNetwork.service.Impl;
+
+import com.challenge.socialNetwork.data.dto.UserDto;
+import com.challenge.socialNetwork.data.model.User;
+import com.challenge.socialNetwork.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public void saveUser(UserDto userDto) {
+
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<UserDto> findAllUsers() {
+        return null;
+    }
 }
