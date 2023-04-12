fa869bc27332442624dc03ecb9032bec80c3347d
reihaneh <reihaneh763@gmail.com>
2023-04-12 14:39:46 +0430

    add RoleServiceImpl class

--- src/main/java/com/challenge/socialNetwork/service/Impl/RoleServiceImpl.java
+++ src/main/java/com/challenge/socialNetwork/service/Impl/RoleServiceImpl.java
@@ -1,2 +1,25 @@
-package com.challenge.socialNetwork.service.Impl;public class RoleServiceImpl {
+package com.challenge.socialNetwork.service.Impl;
+
+import com.challenge.socialNetwork.data.model.Role;
+import com.challenge.socialNetwork.data.repository.RoleRepository;
+import com.challenge.socialNetwork.service.RoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RoleServiceImpl implements RoleService {
+    private RoleRepository roleRepository;
+
+    @Override
+    public Role saveRole(String roleName) {
+        Role role = new Role();
+        role.setName("ROLE_ADMIN");
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public Role findByRoleName(String roleName) {
+        return roleRepository.findByName("ROLE_ADMIN");
+    }
 }
