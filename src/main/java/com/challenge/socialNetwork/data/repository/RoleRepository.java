f19b3825292656122cdda493ced8456fb4e3627b
reihaneh <reihaneh763@gmail.com>
2023-04-12 14:06:35 +0430

    add RoleRepository class

--- src/main/java/com/challenge/socialNetwork/data/repository/RoleRepository.java
+++ src/main/java/com/challenge/socialNetwork/data/repository/RoleRepository.java
@@ -1,4 +1,9 @@
 package com.challenge.socialNetwork.data.repository;
 
-public interface RoleRepository {
+import com.challenge.socialNetwork.data.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
 }
