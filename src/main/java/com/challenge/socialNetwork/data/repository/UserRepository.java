5ba16d09e4e9f1201b81f7512becb60b7c34b8cc
reihaneh <reihaneh763@gmail.com>
2023-04-12 14:05:58 +0430

    add UserRepository class

--- src/main/java/com/challenge/socialNetwork/data/repository/UserRepository.java
+++ src/main/java/com/challenge/socialNetwork/data/repository/UserRepository.java
@@ -1,2 +1,11 @@
-package com.challenge.socialNetwork.data.repository;public class UserRepository {
+package com.challenge.socialNetwork.data.repository;
+
+import com.challenge.socialNetwork.data.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+
 }
