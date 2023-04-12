f43f8578f1049cb0ce39911f9703c6ef9f8338a9
reihaneh <reihaneh763@gmail.com>
2023-04-12 14:04:59 +0430

    add Role class

--- src/main/java/com/challenge/socialNetwork/data/model/Role.java
+++ src/main/java/com/challenge/socialNetwork/data/model/Role.java
@@ -1,2 +1,28 @@
-package com.challenge.socialNetwork.data.model;public class Role {
+package com.challenge.socialNetwork.data.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name="roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
+    private String name;
+
+    @ManyToMany(mappedBy="roles")
+    private List<User> users;
 }
