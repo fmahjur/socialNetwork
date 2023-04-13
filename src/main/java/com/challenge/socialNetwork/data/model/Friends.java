9af6ca726eb1e1dabd78028ac743639c6b55cc0d
reihaneh <reihaneh763@gmail.com>
2023-04-13 12:34:21 +0430

    add Friends class

--- src/main/java/com/challenge/socialNetwork/data/model/Friends.java
+++ src/main/java/com/challenge/socialNetwork/data/model/Friends.java
@@ -1,2 +1,43 @@
-package com.challenge.socialNetwork.data.model;public class Friends {
+package com.challenge.socialNetwork.data.model;
+
+import com.challenge.socialNetwork.data.enaums.FriendshipStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name="friends")
+public class Friends {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    User firstUser;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    User secondUser;
+
+    @CreationTimestamp
+    LocalDateTime friendshipDate;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    FriendshipStatus friendshipStatus;
+
+    public Friends(User firstUser, User secondUser) {
+        this.firstUser = firstUser;
+        this.secondUser = secondUser;
+        this.friendshipStatus = FriendshipStatus.REQUESTED;
+    }
 }
