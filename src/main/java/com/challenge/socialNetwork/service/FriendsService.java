cce9db2074a4e5d5dc90aaf0366d614356e4380e
reihaneh <reihaneh763@gmail.com>
2023-04-13 12:30:31 +0430

    add FriendService class

--- src/main/java/com/challenge/socialNetwork/service/FriendsService.java
+++ src/main/java/com/challenge/socialNetwork/service/FriendsService.java
@@ -1,2 +1,16 @@
-package com.challenge.socialNetwork.service;public interface FriendsService {
+package com.challenge.socialNetwork.service;
+
+import com.challenge.socialNetwork.data.dto.UserDto;
+import com.challenge.socialNetwork.data.model.User;
+
+import java.util.List;
+
+public interface FriendsService {
+    void newFriendRequest(Long id, UserDto friend);
+
+    void acceptedFriendship(Long id, UserDto friend);
+
+    void rejectedFriendship(Long id, UserDto friend);
+
+    List<User> getFriends(Long id);
 }
