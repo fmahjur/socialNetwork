14746972c473ad90558cec745007cb597161d9d9
reihaneh <reihaneh763@gmail.com>
2023-04-13 12:37:05 +0430

    add Config class

--- src/main/java/com/challenge/socialNetwork/config/Config.java
+++ src/main/java/com/challenge/socialNetwork/config/Config.java
@@ -1,6 +1,5 @@
-package ir.maktab.finalprojectphase4.config;
+package com.challenge.socialNetwork.config;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -22,26 +21,4 @@ public class Config {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
-
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername("reihaneh763@gmail.com");
-        mailSender.setPassword("jootwduifeuocrtu");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
-    }
 }
