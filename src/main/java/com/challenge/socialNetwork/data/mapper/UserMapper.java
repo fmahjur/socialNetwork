c1c5f59042090ce37762435d1461f8e1d3856444
reihaneh <reihaneh763@gmail.com>
2023-04-13 12:34:44 +0430

    add UserMapper class

--- src/main/java/com/challenge/socialNetwork/data/mapper/UserMapper.java
+++ src/main/java/com/challenge/socialNetwork/data/mapper/UserMapper.java
@@ -1,2 +1,15 @@
-package com.challenge.socialNetwork.data.mapper;public class UserMapper {
+package com.challenge.socialNetwork.data.mapper;
+
+import com.challenge.socialNetwork.data.dto.UserDto;
+import com.challenge.socialNetwork.data.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User userDtoToModel(UserDto userDto);
+
+    UserDto modelToUserDto(User user);
 }
